bf3bc97351f31612f037946ac99abca3c12c70c4
Sinclair Jaza <39466267+sinclairjaza@users.noreply.github.com>
2023-05-11 10:48:07 +0100

    update de la table service avec ajout des autres tables pour repondre aux besoin service parent et fils

--- Exercice1.sql
+++ Exercice1.sql
@@ -42,19 +42,57 @@ CREATE TABLE family_member (
   FOREIGN KEY (employee_id) REFERENCES employee(id)
 );
 
-/*creation de la table service*/
+/*creation de la table service
+* La table Service stocke les informations sur les différents services de l'entreprise
+*/
 CREATE TABLE service (
   id INT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  manager_id INT NOT NULL,
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  name VARCHAR(50) NOT NULL
 );
 
-/*creation de la table employe service pour la gestion des services par des employes*/
+/*creation de la table employe service
+* La table Employee_Service permet de stocker les relations entre les salariés et les services. 
+* Elle est utilisée pour indiquer les services auxquels chaque salarié appartient
+*/
 CREATE TABLE employee_service (
-  id INT PRIMARY KEY,
   employee_id INT NOT NULL,
   service_id INT NOT NULL,
+  PRIMARY KEY (employee_id, service_id),
   FOREIGN KEY (employee_id) REFERENCES employee(id),
   FOREIGN KEY (service_id) REFERENCES service(id)
+);
+
+/*
+* La table service_relationship stocke les relations entre les différents services de l'entreprise. 
+* Elle est utilisée pour indiquer quels sont les services parents et enfants,
+*/
+CREATE TABLE service_relationship (
+  parent_service_id INT,
+  child_service_id INT,
+  PRIMARY KEY (parent_service_id, child_service_id),
+  FOREIGN KEY (parent_service_id) REFERENCES service(id),
+  FOREIGN KEY (child_service_id) REFERENCES service(id)
+);
+
+/*
+* La table service_employee permet de stocker les relations entre les services et les salariés qui y travaillent. 
+*/
+CREATE TABLE service_employee (
+  service_id INT,
+  employee_id INT,
+  PRIMARY KEY (service_id, employee_id),
+  FOREIGN KEY (service_id) REFERENCES service(id),
+  FOREIGN KEY (employee_id) REFERENCES employee(id)
+);
+
+/*
+* La table employee_manager permet de stocker les relations de management entre les salariés. 
+* Chaque entrée indique que l'employé avec l'ID employee_id est managé par l'employé avec l'ID manager_id.
+*/
+CREATE TABLE employee_manager (
+  employee_id INT,
+  manager_id INT,
+  PRIMARY KEY (employee_id, manager_id),
+  FOREIGN KEY (employee_id) REFERENCES employee(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
